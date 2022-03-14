0374eccf5fdc894412e5690ac0146531d63ffdb0
shirinapr <shirinapr1@gmail.com>
2022-03-14 14:38:07 +0330

    remove: antd from base.

--- src/start.js
+++ src/start.js
@@ -1,43 +1,43 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require("electron");
 
-const path = require('path')
-const url = require('url')
+const path = require("path");
+const url = require("url");
 
-let mainWindow
+let mainWindow;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1440,
+    height: 1024,
     webPreferences: {
       nodeIntegration: true,
     },
-  })
+  });
 
   mainWindow.loadURL(
     process.env.ELECTRON_START_URL ||
       url.format({
-        pathname: path.join(__dirname, '/../public/index.html'),
-        protocol: 'file:',
+        pathname: path.join(__dirname, "/../public/index.html"),
+        protocol: "file:",
         slashes: true,
       })
-  )
+  );
 
-  mainWindow.on('closed', () => {
-    mainWindow = null
-  })
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
 }
 
-app.on('ready', createWindow)
+app.on("ready", createWindow);
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-})
+});
 
-app.on('activate', () => {
+app.on("activate", () => {
   if (mainWindow === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
