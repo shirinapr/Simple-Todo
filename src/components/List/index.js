4c8225957460ca58a5a03e09c4657d5d0cea2578
shirinapr <shirinapr1@gmail.com>
2023-02-13 16:46:46 +0330

    fix: reverted to old version.

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,52 +1,51 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 
-import ModalComp from '../Modal'
-import { useSelector, useDispatch } from 'react-redux'
-import { List, Button, Popconfirm, message, Row, Col } from 'antd'
-import moment from 'moment'
-import './style.css'
-
+import ModalComp from '../Modal';
+import { useSelector, useDispatch } from 'react-redux';
+import { List, Button, Popconfirm, message, Row, Col } from 'antd';
+import moment from 'moment';
+import './style.css';
 
 const ListComp = (props) => {
-  const dispatch = useDispatch()
-  const todo = useSelector((state) => state.todo)
-  const [isVisible, setIsVisible] = useState(false)
-  const currentTopic = useSelector((state) => state.currentTopic)
+  const dispatch = useDispatch();
+  const todo = useSelector((state) => state.todo);
+  const [isVisible, setIsVisible] = useState(false);
+  const currentTopic = useSelector((state) => state.currentTopic);
   useEffect(() => {
-    console.log('component changed')
-  })
+    console.log('component changed');
+  });
   const handleDelete = (id) => {
     dispatch({
       type: 'DELETE_TODO',
       payload: id,
-    })
-    message.success('Click on Yes')
-  }
+    });
+    message.success('Click on Yes');
+  };
 
   const handleEdit = (item) => {
-    message.success('Succeed') //*
+    message.success('Succeed'); //*
     dispatch({
       type: 'ACTIVATE_TODO',
       payload: item.id,
-    })
+    });
     setTimeout(() => {
-      setIsVisible(true)
-    }, 500)
-  }
+      setIsVisible(true);
+    }, 500);
+  };
 
   function cancel(e) {
-    console.log(e)
-    message.error('Canceled') //*
+    console.log(e);
+    message.error('Canceled'); //*
   }
   const func = (a, b) => {
-    return b.date - a.date
-  }
-  let newTodo = todo
+    return b.date - a.date;
+  };
+  let newTodo = todo;
 
   if (currentTopic !== 'all') {
     newTodo = todo.filter((t) => {
-      return t.topic === currentTopic
-    })
+      return t.topic === currentTopic;
+    });
   }
 
   return (
@@ -65,27 +64,31 @@ const ListComp = (props) => {
                 {item.date !== 0 ? moment(item.date).format('ll') : ''}
                 &nbsp;
                 <Popconfirm
-                  title='Are you sure to edit this task?'
+                  title="Are you sure to edit this task?"
                   onConfirm={() => {
-                    handleEdit(item)
+                    handleEdit(item);
                   }}
                   onCancel={cancel}
-                  okText='Yes'
-                  cancelText='No'
+                  okText="Yes"
+                  cancelText="No"
                 >
-                  <Button type='link' className="task-btn">Edit</Button>
+                  <Button type="link" className="edit">
+                    Edit
+                  </Button>
                 </Popconfirm>
                 &nbsp;
                 <Popconfirm
-                  title='Are you sure to delete this task?'
+                  title="Are you sure to delete this task?"
                   onConfirm={() => {
-                    handleDelete(item.id)
+                    handleDelete(item.id);
                   }}
                   onCancel={cancel}
-                  okText='Yes'
-                  cancelText='No'
+                  okText="Yes"
+                  cancelText="No"
                 >
-                  <Button type='link' className="task-btn">Delete</Button>
+                  <Button type="link" className="delete">
+                    Delete
+                  </Button>
                 </Popconfirm>
               </List.Item>
             )}
@@ -93,7 +96,7 @@ const ListComp = (props) => {
         </Col>
       </Row>
     </>
-  )
-}
+  );
+};
 
-export default ListComp
+export default ListComp;
